473a5eead62ba0cc639df9dbf8f29b231b7b735d
Дмитрий Горбунов <bous07@mail.ru>
2023-10-22 01:08:18 +0800

    Adding & using Best Practices

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -2,6 +2,7 @@ global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Server.Data;
 global using Microsoft.AspNetCore.Mvc;
+global using BlazorEcommerce.Server.Services.ProductService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +16,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
