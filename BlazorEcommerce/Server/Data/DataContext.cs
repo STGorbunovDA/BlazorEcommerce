05d9671c2ef116471b2881fc496edf2a126000f5
Дмитрий Горбунов <bous07@mail.ru>
2023-10-22 19:43:18 +0800

    ProductValiants Seeding & Migration

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -8,11 +8,28 @@
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<ProductVariant>()
+               .HasKey(p => new { p.ProductId, p.ProductTypeId });
+
+            modelBuilder.Entity<ProductType>().HasData(
+                   new ProductType { Id = 1, Name = "Default" },
+                   new ProductType { Id = 2, Name = "Paperback" },
+                   new ProductType { Id = 3, Name = "E-Book" },
+                   new ProductType { Id = 4, Name = "Audiobook" },
+                   new ProductType { Id = 5, Name = "Stream" },
+                   new ProductType { Id = 6, Name = "Blu-ray" },
+                   new ProductType { Id = 7, Name = "VHS" },
+                   new ProductType { Id = 8, Name = "PC" },
+                   new ProductType { Id = 9, Name = "PlayStation" },
+                   new ProductType { Id = 10, Name = "Xbox" }
+               );
 
             modelBuilder.Entity<Category>().HasData(
                 new Category
@@ -43,7 +60,6 @@
                        Title = "Слимп",
                        Description = "Семён, смышлёный парнишка 22 лет, сидя в компании приятелей, ненароком заскучал. Развернув одну из старых газет, он обнаружил странное объявление: к некоему Магическому Двору приглашались желающие получить профессию мага-воина. Не подумав, Семён механически прочитал вслух заклинание под объявлением (причём слова «Магический Двор» он спьяну прочитал как «Магический Вор») и оказался на… Вселенском Диске рядом с Магическим вором – живым, говорящим медальоном на цепочке…",
                        ImageUrl = "https://avidreaders.ru/pics/3/8/64838.jpg",
-                       Price = 9.99m
                    },
                    new Product
                    {
@@ -52,7 +68,6 @@
                        Title = "Слимпер",
                        Description = "Изумрудный Мир… Красивое название, не правда ли? А еще вот такие Миры: Тупиковый, Ханский, Мандариновый, Торговый, Выгребной… ну и Исправительный, как же без него? А еще Мир Равновесия, Мир Прокаженных и много всяких прочих. Все эти Истинные Миры располагаются на Вселенском Диске, по которому и скитается в поисках приключений или по необходимости удачливый вор Симеон, бывший «земной» студент, со своим странным другом – медальоном по имени Мар, обеспечивающим Симеону магическое прикрытие…",
                        ImageUrl = "https://avidreaders.ru/pics/0/3/433503.jpeg",
-                       Price = 7.99m
                    },
                    new Product
                    {
@@ -61,14 +76,11 @@
                        Title = "Слиперия",
                        Description = "Пока Семён, вор с магическим прикрытием, искал достойный подарок ко дню коронации Яны, правительницы одного из самых крупных королевств Изумрудного Мира, та внезапно решила выйти замуж за его друга и телохранителя Хайка, воина из клана Болотной Черепахи. Это повергло Семёна в некоторую растерянность, перешедшую в двухнедельный загул. Хорошо, что у него есть Мар, магический медальон, который вытаскивает его из нехороших ситуаций, перебрасывая из одного Мира в другой. Но все когда-нибудь надоедает. Вот и Семён решает наконец отдохнуть и хорошенько подумать, что делать дальше. И тут он получает заказ от главы Вседисковой Гильдии Воров, Мастера Четырех Углов, выкрасть магический жезл – якобы для его личной коллекции. Так Семён знакомится с внучкой этого Мастера – Олией. К ним неожиданно присоединяется Хайк. Приключения продолжаются…",
                        ImageUrl = "https://avidreaders.ru/pics/0/0/166100.jpg",
-                       Price = 6.99m
-
                    },
                    new Product
                    {
                        Id = 4,
                        CategoryId = 2,
-                       Price = 4.99m,
                        Title = "Матрица",
                        Description = "Жизнь Томаса Андерсона разделена на две части: днём он — самый обычный офисный работник, получающий нагоняи от начальства, а ночью превращается в хакера по имени Нео, и нет места в сети, куда он бы не смог проникнуть. Но однажды всё меняется. Томас узнаёт ужасающую правду о реальности",
                        ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/cf1970bc-3f08-4e0e-a095-2fb57c3aa7c6/300x450",
@@ -77,7 +89,6 @@
                    {
                        Id = 5,
                        CategoryId = 2,
-                       Price = 3.99m,
                        Title = "Назад в будущее",
                        Description = "Подросток Марти с помощью машины времени, сооружённой его другом-профессором доком Брауном, попадает из 80-х в далекие 50-е. Там он встречается со своими будущими родителями, ещё подростками, и другом-профессором, совсем молодым.",
                        ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/73cf2ed0-fd52-47a2-9e26-74104360786a/300x450",
@@ -86,7 +97,6 @@
                    {
                        Id = 6,
                        CategoryId = 2,
-                       Price = 2.99m,
                        Title = "История игрушек",
                        Description = "В спальне Энди его игрушки начинают жить своей жизнью, как только он выходит из комнаты. В день рождения мальчика царит паника — все боятся появления новой игрушки, которая перетянет внимание на себя. И только любимчик Эдди, ковбой Вуди, ни о чем не переживает. Однако, когда мальчик получает в подарок фигурку астронавта Базза Лайтера, Вуди очень быстро оказывается забыт. И тогда ковбой решает вернуть себе первое место в сердце Энди.",
                        ImageUrl = "https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/46fd178b-9a99-4974-a7b5-4b56bbc4b08e/300x450",
@@ -97,7 +107,6 @@
                        Id = 7,
                        CategoryId = 3,
                        Title = "Half-Life 2",
-                       Price = 49.99m,
                        Description = "Продолжение легендарного приключенческого шутера с видом от первого лица. Вам вновь предстоит поднять монтировку ученого-исследователя Гордона Фримена, который обнаруживает себя на наводненной чужаками Земле, ресурсы которой стремительно опустошаются, а население вырождается. Фримену навязана неизбежная роль спасителя человечества от того зла, которое он выпустил на свободу в Черной Мезе.",
                        ImageUrl = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
 
@@ -107,7 +116,6 @@
                        Id = 8,
                        CategoryId = 3,
                        Title = "Diablo II",
-                       Price = 9.99m,
                        Description = "Компьютерная игра в жанре Action/RPG, разработанная американской компанией Blizzard North и выпущенная Blizzard Entertainment для Windows и Mac OS в 2000 году. Это вторая часть серии Diablo, продолжающая события игры Diablo. В соответствии с сюжетом игры, выполненной в духе тёмного фэнтези, вымышленный мир Санктуарий подвергается нашествию демонов, которым противостоит управляемый игроком персонаж. Как и предыдущая игра серии, Diablo II содержит упрощенный по сравнению с компьютерными ролевыми играми, но увлекательный геймплей, предлагая игроку уничтожать толпы противников и вознаграждая его приростом характеристик и предметами экипировки для персонажа.",
                        ImageUrl = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
                    },
@@ -115,7 +123,6 @@
                    {
                        Id = 9,
                        CategoryId = 3,
-                       Price = 14.99m,
                        Title = "Day of the Tentacle",
                        Description = "Графическая приключенческая игра 1993 года, разработанная и изданная LucasArts. Это продолжение вышедшей в 1987 году игры Maniac Mansion. Сюжет повествует о Бернарде Бернулли и его друзьях Хоги и Лаверне, которые пытаются остановить злое Фиолетовое Щупальце - разумное, бестелесное щупальце - от захвата мира.",
                        ImageUrl = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
@@ -124,7 +131,6 @@
                    {
                        Id = 10,
                        CategoryId = 3,
-                       Price = 159.99m,
                        Title = "Xbox",
                        Description = "Домашняя игровая консоль, разработанная и выпущенная американской корпорацией Microsoft, первая в одноимённой серии игровых приставок Xbox. Выход Xbox в США и Канаде состоялся в ноябре 2001 года, в остальном мире - в 2002 году. ",
                        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
@@ -133,12 +139,124 @@
                    {
                        Id = 11,
                        CategoryId = 3,
-                       Price = 79.99m,
                        Title = "Super Nintendo Entertainment System",
                        Description = "16-битная игровая приставка, выпущенная компанией Nintendo в США, Бразилии, Европе и Австралии. В Японии она известна под названием Super Famicom (яп. スーパーファミコン су:па:фамикон), сокращение от Super Family Computer, или SFC. ",
                        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
                    }
                    );
+
+            modelBuilder.Entity<ProductVariant>().HasData(
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 2,
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 3,
+                    Price = 7.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 1,
+                    ProductTypeId = 4,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 2,
+                    ProductTypeId = 2,
+                    Price = 7.99m,
+                    OriginalPrice = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 3,
+                    ProductTypeId = 2,
+                    Price = 6.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 5,
+                    Price = 3.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 6,
+                    Price = 9.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 4,
+                    ProductTypeId = 7,
+                    Price = 19.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 5,
+                    ProductTypeId = 5,
+                    Price = 3.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 6,
+                    ProductTypeId = 5,
+                    Price = 2.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 8,
+                    Price = 19.99m,
+                    OriginalPrice = 29.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 9,
+                    Price = 69.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 7,
+                    ProductTypeId = 10,
+                    Price = 49.99m,
+                    OriginalPrice = 59.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 8,
+                    ProductTypeId = 8,
+                    Price = 9.99m,
+                    OriginalPrice = 24.99m,
+                },
+                new ProductVariant
+                {
+                    ProductId = 9,
+                    ProductTypeId = 8,
+                    Price = 14.99m
+                },
+                new ProductVariant
+                {
+                    ProductId = 10,
+                    ProductTypeId = 1,
+                    Price = 159.99m,
+                    OriginalPrice = 299m
+                },
+                new ProductVariant
+                {
+                    ProductId = 11,
+                    ProductTypeId = 1,
+                    Price = 79.99m,
+                    OriginalPrice = 399m
+                }
+            );
         }
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -10,7 +10,7 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -18,8 +18,21 @@
 
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
+            /*
+                * Include(p => p.Variants) говорит EF получить не только основные данные о продукте, но и связанные данные 
+                  о вариантах этого продукта. Вместо по умолчания "ленивой" загрузки, которая бы получила эти данные при 
+                  первом обращении к ним, метод Include применяет "немедленную" загрузку, чтобы получить все эти данные сразу. 
+                  Это полезно, если вы знаете, что вам понадобятся эти данные, и хотите избежать дополнительного обращения 
+                  к базе данных немного позже.
+
+                * .ThenInclude(v => v.ProductType) говорит EF не только получить данные о вариантах, но и связанные с вариантами 
+                  данные о типах продуктов. Это еще одна немедленная загрузка.
+            */
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
@@ -39,6 +52,7 @@
             {
                 Data = await _context.Products
                     .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                    .Include(p => p.Variants)
                     .ToListAsync()
             };
 
