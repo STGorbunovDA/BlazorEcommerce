8536e473e7db1083e705788332fb2019b8000ecd
Дмитрий Горбунов <bous07@mail.ru>
2023-10-23 08:50:39 +0800

    Implemented the Product Search Feature. Custom Layout.

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -3,6 +3,7 @@
     public class ProductService : IProductService
     {
         public event Action ProductsChanged;
+        public string Message { get; set; } = "Loading products...";
 
         private readonly HttpClient _http;
 
@@ -25,6 +26,23 @@
                 Products = result.Data;
 
             ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                 .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "No products found.";
+            ProductsChanged?.Invoke();
         }
     }
 }
