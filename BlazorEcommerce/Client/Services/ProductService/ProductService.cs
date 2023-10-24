80ee64d470da8fc77f55e4e1ac2ff763821f9e14
Дмитрий Горбунов <bous07@mail.ru>
2023-10-24 22:24:08 +0800

    Pagination for Product Search.

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -4,6 +4,9 @@
     {
         public event Action ProductsChanged;
         public string Message { get; set; } = "Loading products...";
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+        public string LastSearchText { get; set; } = string.Empty;
 
         private readonly HttpClient _http;
 
@@ -25,6 +28,12 @@
             if (result != null && result.Data != null)
                 Products = result.Data;
 
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if (Products.Count == 0)
+                Message = "No products found";
+
             ProductsChanged.Invoke();
         }
 
@@ -35,12 +44,17 @@
             return result.Data;
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText, int page)
         {
+            LastSearchText = searchText;
             var result = await _http
-                 .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+                 .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
             if (result != null && result.Data != null)
-                Products = result.Data;
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
             if (Products.Count == 0) Message = "No products found.";
             ProductsChanged?.Invoke();
         }
